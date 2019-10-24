fe961f928237e4f4f731b13ea7f1502f5679d64f
mnottveit <mnottveit@gmail.com>
2019-10-24 13:49:37 +0200

    begynt på underklassene

--- behaviour.py
+++ behaviour.py
@@ -65,7 +65,7 @@ class AvoidWall(Behaviour):
         self.sense_and_act()
 
     def sense_and_act(self):
-        #TODO x = sensor-readings (distance to wall/object)
+        # TODO x = sensor-readings (distance to wall/object)
         self.match_degree = 1/(exp(x))
         self.update_weight(self.priority, self.match_degree)
         self.update_mr("B", 50)
@@ -79,7 +79,7 @@ class AvoidLineCrossing(Behaviour):
         self.match_degree = 0
 
     def sense_and_act(self):
-        #TODO x = sensor-readings (distance to wall/object)
+        # TODO check if robot is on line
         self.match_degree = 1
         self.update_weight(self.priority, self.match_degree)
         self.update_mr("R", 180)
@@ -97,7 +97,8 @@ class FollowRed(Behaviour):
         self.match_degree = 0
 
     def sense_and_act(self):
-        #TODO x = sensor-readings (distance to wall/object)
+        # TODO x = sensor-readings (distance to wall/object)
+        # TODO get information about where it is red
         self.match_degree = 1
         self.update_weight(self.priority, self.match_degree)
         self.update_mr("R", 180)

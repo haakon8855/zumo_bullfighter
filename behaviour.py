1dc05381bd2d2fefd2bb30f102835e632a548683
mnottveit <mnottveit@gmail.com>
2019-10-24 11:51:57 +0200

    laget init

--- behaviour.py
+++ behaviour.py
@@ -4,5 +4,14 @@
 class Behaviour():
     '''Behaviour class for recommending actuator action'''
 
-    def __init__(self):
-        self.bbcon = None #Må endres
+    def __init__(self, priority):
+        pass
+        #TODO self.bbcon = None
+        #TODO self.sensob_list = []
+        #TODO self.sensob_list.append(sensob)
+        self.motor_recmmendations = [] # [Direction, speed%], [Turn_direction, degrees]
+        self.active_flag = True
+        self.halt_request = False
+        self.priority = priority
+
+

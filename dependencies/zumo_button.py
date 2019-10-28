5aa8be30f8f5949868c2d4a37b5e393287053604
Håkon Anders <haakon8855@users.noreply.github.com>
2019-10-28 09:36:06 +0100

    zumo_button wrapper has been linted

--- dependencies/zumo_button.py
+++ dependencies/zumo_button.py
@@ -1,17 +1,24 @@
-__author__ = 'keithd'
+'''keithd'''
+
 import wiringpi as wp
+
 
 class ZumoButton():
     '''Wrapper class for zumo push button'''
 
     def __init__(self):
-        wp.wiringPiSetupGpio()
-        wp.pinMode(22, 0)
-        wp.pullUpDnControl(22, 2)
+        self.wpi = wp
+
+    def setup(self):
+        '''Sets up the necessary settings for the push button'''
+        self.wpi.wiringPiSetupGpio()
+        self.wpi.pinMode(22, 0)
+        self.wpi.pullUpDnControl(22, 2)
+
 
     def wait_for_press(self):
         '''Runs an infinite loop until the push button is pressed'''
-        read_val = wp.digitalRead(22)
+        read_val = self.wpi.digitalRead(22)
         while read_val:
-            read_val = wp.digitalRead(22)
+            read_val = self.wpi.digitalRead(22)
         print("Button pressed!!")

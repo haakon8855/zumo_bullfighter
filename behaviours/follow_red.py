b79be84438e6d89e29750102a4475ba7767bdad0
Håkon Anders <haakon8855@users.noreply.github.com>
2019-10-28 15:43:58 +0100

    Removed todo from follow_red

--- behaviours/follow_red.py
+++ behaviours/follow_red.py
@@ -17,7 +17,6 @@ class FollowRed(Behaviour):
         self.sense_and_act()
 
     def sense_and_act(self):
-        # TODO get information about where it is red
         values = self.sensob_camera.update()
         if values[0] > values[1] and values[0] > values[2]:
             self.update_mr("L", 25)
